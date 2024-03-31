0dc40f7ce96676a5a72b5f957f03dfaa9b747a08
Ian <irausch3@gatech.edu>
2024-03-31 18:18:27 -0400

    comment code

--- multinomialnaivebayes.py
+++ multinomialnaivebayes.py
@@ -19,11 +19,14 @@ class EmotionClassifier:
 
     @staticmethod
     def preprocess_text(text):
+        # using text normalization for preprocessing
         text = text.lower()
         text = re.sub(r"[^a-zA-Z0-9\s]", '', text)
-
+        
+        # tokenization for preprocessing
         tokens = word_tokenize(text)
 
+        # negation handling for preprocessing
         negation_terms = {'not', 'no', 'never', 'none'}
         tokens = ['not_' + tokens[i+1] if tokens[i] in negation_terms and i+1 < len(tokens) else token 
                   for i, token in enumerate(tokens)]
@@ -31,25 +34,31 @@ class EmotionClassifier:
         return ' '.join(tokens)
 
     def train(self, X_train, y_train):
+        # convert the text data into a Bag-of-Words model that 
+        # transforms text into a set of frequency counts of words 
         X_train_vect = self.vectorizer.fit_transform(X_train)
+        # using multinomial nb 
         self.model_nb.fit(X_train_vect, y_train)
 
     def predict(self, X):
+        # same as training using bag of words model 
         X_vect = self.vectorizer.transform(X)
         return self.model_nb.predict(X_vect)
 
-
+# load and preprocess the data
 df = pd.read_csv("text.csv")
 df['processed_text'] = df['text'].apply(EmotionClassifier.preprocess_text)
 
 classifier = EmotionClassifier()
 
 def cross_validate_evaluate(X, y):
+    # using cross validation 
     kf = KFold(n_splits=5, random_state=42, shuffle=True)
     acc_scores, prec_scores, f1_scores = [], [], []
     aggregated_cm = np.zeros((6, 6))
 
     for train_index, test_index in kf.split(X):
+        # split the data into training and testing sets
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
--- naivebayes.py
+++ naivebayes.py
@@ -17,6 +17,8 @@ from sklearn.metrics import (
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.naive_bayes import GaussianNB
 
+# Downloading necessary NLTK data
+nltk.download('punkt')
 
 class EmotionClassifier:
     def __init__(self):
@@ -25,11 +27,14 @@ class EmotionClassifier:
 
     @staticmethod
     def preprocess_text(text):
+        # using text normalization for preprocessing
         text = text.lower()
         text = re.sub(r"[^a-zA-Z0-9\s]", '', text)
 
+        # tokenization for preprocessing
         tokens = word_tokenize(text)
-
+        
+        # negation handling for preprocessing
         negation_terms = {'not', 'no', 'never', 'none'}
         tokens = ['not_' + tokens[i+1] if tokens[i] in negation_terms and i+1 < len(tokens) else token 
                   for i, token in enumerate(tokens)]
@@ -37,16 +42,18 @@ class EmotionClassifier:
         return ' '.join(tokens)
 
     def train_word2vec(self, texts):
+        # train a Word2Vec model
         self.model_w2v = Word2Vec(texts, vector_size=100, window=5, min_count=1)
 
     def tweet_to_vector(self, tweet):
+        # convert data to vectors using word2vec model
         words = tweet.split()
         word_vectors = [self.model_w2v.wv[word] for word in words if word in self.model_w2v.wv]
         return np.mean(word_vectors, axis=0) if len(word_vectors) > 0 else np.zeros(100)
 
     def train(self, X_train, y_train):
+        # convert text to vectors and train Naive Bayes model
         self.train_word2vec([text.split() for text in X_train])
-
         X_train_vect = np.array([self.tweet_to_vector(tweet) for tweet in X_train])
 
         # Training Naive Bayes
@@ -54,21 +61,24 @@ class EmotionClassifier:
         self.model_nb.fit(X_train_vect, y_train)
 
     def predict(self, X):
+        # same as training using word2vec model 
         X_vect = np.array([self.tweet_to_vector(tweet) for tweet in X])
         return self.model_nb.predict(X_vect)
 
-
+# load and preprocess the data
 df = pd.read_csv("text.csv")
 df['processed_text'] = df['text'].apply(EmotionClassifier.preprocess_text)
 
 classifier = EmotionClassifier()
 
 def cross_validate_evaluate(X, y):
+    # using cross validation 
     kf = KFold(n_splits=5, random_state=42, shuffle=True)
     acc_scores, prec_scores, f1_scores = [], [], []
     aggregated_cm = np.zeros((6, 6))
 
     for train_index, test_index in kf.split(X):
+        # split the data into training and testing sets
         X_train, X_test = X[train_index], X[test_index]
         y_train, y_test = y[train_index], y[test_index]
 
